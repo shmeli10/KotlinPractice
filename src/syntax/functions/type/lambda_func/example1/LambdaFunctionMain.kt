23f24b8b8f6e991fdba2817ceefec690962dedf0
shmeli10 <shmeli10@gmail.com>
2020-07-21 18:31:43 +0300

    New lambda example code.

--- src/syntax/functions/type/lambda_func/example1/LambdaFunctionMain.kt
+++ src/syntax/functions/type/lambda_func/example1/LambdaFunctionMain.kt
@@ -1,4 +1,4 @@
-package syntax.functions.type.lambda_func
+package syntax.functions.type.lambda_func.example1
 
 fun main(args: Array<String>) {
     val myLambda: (String) -> Unit = {s: String -> print(s)}
